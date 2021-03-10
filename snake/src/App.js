b8b634f4b6f693d2253ce67ccf48c98edd1e6417
toneiojimz <ajimeneza77@gmail.com>
2021-03-10 13:27:46 -0800

    added variables for logic

--- snake/src/App.js
+++ snake/src/App.js
@@ -3,6 +3,32 @@ import './App.css';
 import React,{Component, Fragment} from 'react'
 
 function App() {
+
+  let grid = document.querySelector(".grid"); 
+  let popup = document.querySelector(".popup"); 
+  let playAgain = document.querySelector(".playAgain"); 
+  let scoreDisplay = document.querySelector(".scoreDisplay"); 
+  let left = document.querySelector(".left");
+  let bottom = document.querySelector(".bottom"); 
+  let right = document.querySelector(".right"); 
+  let up = document.querySelector(".top"); 
+  let width=15; 
+  let currentIndex = 0 
+  let appleIndex=0 
+  let currentSnake=[2,1,0] 
+  let direction =1 
+  let score = 0 
+  let speed = 0.8 
+  let intervalTime =0 
+  let interval =0
+
+
+  
+
+
+
+
+
   return (
     // <Fragment> to render, we can use React.Fragment, Fragment or <></>
       <>
