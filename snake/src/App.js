bb627237f304a1961b695ce1cbb11876927698d2
toneiojimz <ajimeneza77@gmail.com>
2021-03-09 16:41:35 -0800

    fixed hmtl markup with react.fragment

--- snake/src/App.js
+++ snake/src/App.js
@@ -1,21 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react'
 
 function App() {
   return (
+    <React.Fragment>
     <h1>Snake Game</h1>
     <div class="scoreDisplay"></div>
     <div class="grid"></div>
     <div class="button">
-      <button class="top">Top</button>
-      <button class="bottom">Bottom</button>
-      <button class="left">Left</button>
-      <button class="right">Right</button>
+        <button class="top">Top</button>
+        <button class="bottom">Bottom</button>
+        <button class="left">Left</button>
+        <button class="right">Right</button>
     </div>
     <div class="popup">
-      <button class="playAgain">Play Again</button>
-
+        <button class="playAgain">Play Again</button>
     </div>
+    </React.Fragment>
+    
   );
 }
 
